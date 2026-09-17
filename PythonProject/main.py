a3847f0b330e1e1a249caa9004d68bbc6b78192e
dnostra12 <dnostra12@naver.com>
2026-09-17 09:31:13 +0900

    Path import 추가

--- PythonProject/main.py
+++ PythonProject/main.py
@@ -2,6 +2,7 @@ import streamlit as st
 import pandas as pd
 import matplotlib.pyplot as plt
 from matplotlib.ticker import StrMethodFormatter
+from pathlib import Path
 
 
 # =====================================================
